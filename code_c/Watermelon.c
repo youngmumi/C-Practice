9a8dee7d3576b1f7df9d95906a216904fc630493
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2023-09-06 14:10:34 +0900

    Update Watermelon.c

--- code_c/Watermelon.c
+++ code_c/Watermelon.c
@@ -1,20 +1,20 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12922
 
+
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
 char* solution(int n) {
     // 리턴할 값은 메모리를 동적 할당해주세요.
-    char* answer = (char*)malloc(n*strlen("수"));
-    
-    strcpy(answer,"");
-    
-    for(int i=0; i<n; i++){
-        if(i%2==0) strcat(answer, "수");
-        else strcat(answer, "박");
+    char *add1 = "수";
+    char *add2 = "박";
+    int len = strlen(add1);
+    char *answer = (char*)malloc(n*len);
+    for(int i=0; i<n; i++) {
+        strcpy(&answer[i*len], i%2==0 ? add1 : add2);
     }
-    
 
     return answer;
 }
