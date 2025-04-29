98b41218df40efa577eb07debd5eab104de1533b
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2025-04-29 11:28:20 +0900

    Add files via upload

---
+++ auto_programming/20210093_241217_01.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main() {
+	int i;
+	int Array[5] = {};
+
+	printf("** 정수 5개를 입력하세요 **\n\n");
+
+	for (i = 0; i < 5; i++) {
+		printf("%d번째 정수를 입력하세요: ", i + 1);
+		scanf_s("%d", &Array[i]);
+	}
+
+	printf("\n");
+	printf("** 입력한 정수의 역순 출력 **\n");
+
+	for (i = 4; i >= 0; i--) {
+		printf("%d번째 입력한 정수: %d", i + 1, Array[i]);
+		printf("\n");
+	}
+
+	return 0;
+}
---
+++ auto_programming/20210093_241217_02.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+int main() {
+	int i, j;
+	int start, end;
+	int tmp;
+
+	while (1) {
+		printf("구구단의 시작 단을 입력하세요: ");
+		scanf_s("%d", &start);
+		printf("구구단의 끝 단을 입력하세요: ");
+		scanf_s("%d", &end);
+
+		if ((start<2||start>9) || (end < 2 || end > 9)) {
+			printf("\n");
+			printf("구구단의 범위를 잘못 입력하였습니다.\n다시 입력해 주세요.\n");
+		}else {
+			break;
+		}
+
+		printf("\n");
+	}
+	
+	if (start > end) {
+		tmp = start;
+		start = end;
+		end = tmp;
+	}
+
+	for (i = 2; i <= 9; i++) {
+		for (j = start; j <= end; j++) {
+			printf("%d x %d = %d", j, i, i * j);
+			printf("\t");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
